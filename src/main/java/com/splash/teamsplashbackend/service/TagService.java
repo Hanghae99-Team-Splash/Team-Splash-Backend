0b369658d7867da0d21c68584cca45dc89a564a7
mylhb9 <mylhb9@naver.com>
2021-12-13 21:12:41 +0900

    feat/feature tag 태그명 고유 값 entity단 service 처리 완료

--- src/main/java/com/splash/teamsplashbackend/service/TagService.java
+++ src/main/java/com/splash/teamsplashbackend/service/TagService.java
@@ -4,8 +4,11 @@ package com.splash.teamsplashbackend.service;
 import com.splash.teamsplashbackend.dto.tag.TagRequestDto;
 import com.splash.teamsplashbackend.model.Tag;
 import com.splash.teamsplashbackend.repository.TagRepository;
+import com.splash.teamsplashbackend.validator.TagValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -13,7 +16,11 @@ public class TagService {
     private final TagRepository tagRepository;
     //태그 저장
     public void saveTag(TagRequestDto tagRequestDto) {
+        List<Tag> tags = tagRepository.findAll();
+        TagValidator.checkTagName(tagRequestDto, tags);
         tagRepository.save(new Tag(tagRequestDto.getTagname()));
     }
+
+
 
 }
