6f36844aa51f95b7e47704ffb23854ab570854ba
mylhb9 <mylhb9@naver.com>
2021-12-13 22:00:49 +0900

    확인용 푸시

--- src/main/java/com/splash/teamsplashbackend/dto/tag/TagResponseDto.java
+++ src/main/java/com/splash/teamsplashbackend/dto/tag/TagResponseDto.java
@@ -1,6 +1,7 @@
 package com.splash.teamsplashbackend.dto.tag;
 
 
+import com.splash.teamsplashbackend.dto.photoBoard.PhotoBoardResponseDto;
 import lombok.Getter;
 
 import java.util.List;
@@ -9,7 +10,7 @@ import java.util.List;
 public class TagResponseDto {
     private String tagname;
 
-//    private List<PhotoBoardResponseDto> photoboards;
+    private List<PhotoBoardResponseDto> photoboards;
 
 
 }
