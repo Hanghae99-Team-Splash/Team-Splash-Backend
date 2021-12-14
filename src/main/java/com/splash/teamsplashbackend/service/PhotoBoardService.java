db958fc885f6854002688a943a50852e9e7b455e
naocya@naver.com <naocya@naver.com>
2021-12-15 01:28:51 +0900

    feat/User 테스트코드 작성

--- src/main/java/com/splash/teamsplashbackend/service/PhotoBoardService.java
+++ src/main/java/com/splash/teamsplashbackend/service/PhotoBoardService.java
@@ -36,7 +36,7 @@ public class PhotoBoardService {
         PhotoBoard post = PhotoBoard.builder()
                         .img("https://images.unsplash.com/photo-1639353434411-088270055340?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80")
                         .location(photoBoardRequestDto.getLocation())
-                        .description(photoBoardRequestDto.getLocation())
+                        .description(photoBoardRequestDto.getDescription())
                         .tagname(photoBoardRequestDto.getTagname())
                         .user(user)
                         .build();
